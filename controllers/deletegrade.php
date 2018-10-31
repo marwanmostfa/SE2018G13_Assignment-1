35ae12fd3a95290a5d0d75d685d1ec2cc82c0dd1
Ibrahim Amr <36275498+IbrahimAmrIbrahim@users.noreply.github.com>
2018-10-31 18:52:35 +0200

    Add files via upload

---
+++ controllers/deletegrade.php
@@ -0,0 +1,10 @@
+<?php
+
+
+header('Content-Type: application/json; charset=utf-8');
+include_once("../models/grade.php");
+Database::connect('school', 'root', '');
+$grades = new Grade($_GET['id']);
+$grades->delete();
+echo json_encode(['status' => 1]);
+?>
