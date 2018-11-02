c716c74495b4ad26c14ef940f55b863037c76b9f
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2018-11-02 13:49:03 +0200

    Sort Students with Ajax.

---
+++ controllers/studentsort.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+include_once("../models/student.php");
+Database::connect('school', 'root', '');
+$student = Student::all($_GET['keyword'], $_GET['column'], $_GET['order']);
+
+echo json_encode($student);
+?>
